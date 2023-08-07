4cdc660b0f250b27c5efccc360375bba9bd6b8b7
charles <cgiguere@iname.com>
2023-08-07 16:33:16 -0400

    removed DMA code

--- animated_gif_memory.ino
+++ animated_gif_memory.ino
@@ -1,11 +1,7 @@
 // Animated GIF with Round Display
 //
 // ESP32 40MHz SPI single frame rendering performance
-// Note: no DMA performance gain on smaller images or transparent pixel GIFs
-//
-#define USE_DMA       // ESP32 ~1.25x single frame rendering performance boost for badgers.h
-                      // Note: Do not use SPI DMA if reading GIF images from SPI SD card on same bus as TFT
-#define NORMAL_SPEED  // Comment out for rame rate for render speed test
+
 #include <SPI.h>
 #include <TFT_eSPI.h>
 #include <AnimatedGIF.h>
@@ -24,8 +20,8 @@ AnimatedGIF gif;
 
 // Uncomment the image to display
 //
-#define GIF_IMAGE hyperspace
-// #define GIF_IMAGE nostromo
+// #define GIF_IMAGE hyperspace
+#define GIF_IMAGE nostromo
 // #define GIF_IMAGE darthvader
 // #define GIF_IMAGE hud_1
 // #define GIF_IMAGE hud_2
@@ -38,9 +34,6 @@ TFT_eSPI tft = TFT_eSPI();
 void setup() {
   Serial.begin(115200);
   tft.begin();
-#ifdef USE_DMA
-  tft.initDMA();
-#endif
   tft.setRotation(2);
   tft.fillScreen(TFT_BLACK);
   gif.begin(BIG_ENDIAN_PIXELS);
@@ -61,25 +54,3 @@ void loop()
     tft.endWrite(); // Release TFT chip select for other SPI devices
   }
 }
-#else // Test maximum rendering speed
-void loop()
-{
-  long lTime = micros();
-  int iFrames = 0;
-  if (gif.open((uint8_t *)GIF_IMAGE, sizeof(GIF_IMAGE), GIFDraw))
-  {
-    tft.startWrite(); // For DMA the TFT chip slect is locked low
-    while (gif.playFrame(false, NULL))
-    {
-      // Each loop renders one frame
-      iFrames++;
-      yield();
-    }
-    gif.close();
-    tft.endWrite(); // Release TFT chip select for other SPI devices
-    lTime = micros() - lTime;
-    Serial.print(iFrames / (lTime / 1000000.0));
-    Serial.println(" fps");
-  }
-}
-#endif
